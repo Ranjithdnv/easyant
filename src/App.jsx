9bd74a053521a0260aa0c5aec49333cf3bc2edb9
Ranjith <rith8596@gmail.com>
2025-08-05 10:47:05 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,21 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import C1 from "./components/c1";
+
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="p-4">
+          <Routes></Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
